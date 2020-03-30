9b0d95064b685c80dfc333b8192f3699cdf4ed76
Serhii Nomerovskiy <nomerushka@gmail.com>
2020-03-30 22:11:43 +0300

    [#14] configured route

--- HomeCloud.ClientApp/cloud-home-ui-app/src/app/app-routing.module.ts
+++ HomeCloud.ClientApp/cloud-home-ui-app/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AllItemsComponent } from './components/all-items/all-items.component';
+import { VideosComponent } from './components/videos/videos.component';
+import { BooksComponent } from './components/books/books.component';
+import { PhotoComponent } from './components/photo/photo.component';
+import { DocumentComponent } from './components/document/document.component';
+import { NavPanelComponent } from './components/nav-panel/nav-panel.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'allcontent', component: AllItemsComponent},
+  { path: 'book', component: BooksComponent},
+  { path: 'video', component: VideosComponent},
+  { path: 'photo', component: PhotoComponent},
+  { path: 'document', component: DocumentComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- HomeCloud.ClientApp/cloud-home-ui-app/src/app/app.module.ts
+++ HomeCloud.ClientApp/cloud-home-ui-app/src/app/app.module.ts
@@ -11,11 +11,23 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { NavPanelComponent } from './components/nav-panel/nav-panel.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { AllItemsComponent } from './components/all-items/all-items.component';
+import { VideosComponent } from './components/videos/videos.component';
+import { BooksComponent } from './components/books/books.component';
+import { PhotoComponent } from './components/photo/photo.component';
+import {DocumentComponent} from './components/document/document.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavPanelComponent
+    NavPanelComponent,
+    LoginPageComponent,
+    AllItemsComponent,
+    VideosComponent,
+    BooksComponent,
+    PhotoComponent,
+    DocumentComponent
   ],
   imports: [
     BrowserModule,
